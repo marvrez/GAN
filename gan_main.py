8bc897450370e38bd94719dc00dd9774620ef4af
Marvin Reza <marvinreza@outlook.com>
2018-02-24 17:42:15 +0100

    Implement functionality to save result images from training

--- gan_main.py
+++ gan_main.py
@@ -13,13 +13,15 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--batch_size', type=int, default=16,
+    parser.add_argument('--batch_size', type=int, default = 16,
         help='Batch size for the networks')
-    parser.add_argument('--epochs', type=int, default=1000,
+    parser.add_argument('--epochs', type=int, default = 1000,
         help='Epochs for the networks')
-    parser.add_argument('--output_folder', type=str, default='GAN_OUT',
-        help='Output folder to save the image results')
-    parser.add_argument('--input_dim', type=int, default=10,
+    parser.add_argument('--output_dir', type=str, default = 'GAN_OUT',
+        help='Output directory to save the image results.')
+    parser.add_argument('--visualize', type=str, default = 0,
+        help='Set to 1 if you want to visualize the results while training.')
+    parser.add_argument('--input_dim', type=int, default = 100,
         help='Input dimension for the generator.')
 
     args = parser.parse_args()
@@ -27,6 +29,8 @@ def main():
     #load MNIST dataset and preprocess data
     (x_train, y_train), (_, _) = mnist.load_data()
     x_train = preprocess_data(x_train)
+
+    train(args, data)
 
 if __name__ == "__main__":
     main()
--- gan_train.py
+++ gan_train.py
@@ -17,3 +17,23 @@ def preprocess_data(data):
 # Get x from the image distribution
 def get_x(x_train, index, BATCH_SIZE):
     return x_train[index * BATCH_SIZE:(index + 1) * BATCH_SIZE]
+
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width, height = int(math.sqrt(num)), int(math.ceil(float(num) / width))
+    shape = generated_images.shape[2:]
+    image = np.zeros((height * shape[0], width * shape[1]),
+                      dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index / width)
+        j = index % width
+        image[i * shape[0]:(i + 1) * shape[0],
+              j * shape[1]:(j + 1) * shape[1]] = img[0, :, :]
+    return image
+
+def save_images(generated_images, output_dir, epoch, index):
+    image = combine_images(generated_images)
+    image = image * 127.5 + 127.5
+    Image.fromarray(image.astype(np.uint8)).save(
+        output_dir + '/' + str(epoch) + "_" + str(index) + ".png")
